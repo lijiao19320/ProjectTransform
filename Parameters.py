8b779e4746ff8f4b58e59c5750878884baef6e6a
chillpen <jedgbe@hotmail.com>
2016-07-05 23:40:37 +0800

    20160705-2240

--- Parameters.py
+++ Parameters.py
@@ -5,3 +5,18 @@ class ProjParameters(object):
     SrcProj = Proj(proj='longlat',ellps='WGS84')
 
     DstProj = Proj(proj='lcc',ellps='WGS84')
+
+    # def getV(self):
+    #     return self.__V
+    #
+    # def setV(self, value):
+    #     self.__V = value
+    #
+    # V = property(getV, setV)
+
+    __ProjRange = {0,0,0,0}
+
+    def SetProjRange(self,minlat,maxlat,minlon,maxlon):
+        self.__ProjRange = {minlat,maxlat,minlon,maxlon}
+        return
+
